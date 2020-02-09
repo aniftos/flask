3fa3b39f3620b5cf2ed7c17be3a6a9648526229c
george <giorgosaniftos@gmail.com>
2020-02-09 19:20:41 +0000

    create registration and login forms

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__) # __name__ file name
+
+app.config['SECRET_KEY'] = 'ceaa0358f6db721ad45b2e49fce2e7b3'
 
 posts = [
     {
@@ -26,6 +29,17 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title = 'About')
+
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title = 'Register', form = form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title = 'Login', form = form)
 
 
 if __name__ == "__main__":
