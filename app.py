ab4c1fad80cef0b6984ad3d8399391c38bce1f17
aniftos <giorgosaniftos@gmail.com>
2020-03-06 16:52:13 +0000

    updated login and register tmplt

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for
 from forms import RegistrationForm, LoginForm
+from flask import flash, redirect
 
 app = Flask(__name__) # __name__ file name
 
@@ -23,7 +24,7 @@ posts = [
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html',posts=posts)
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
@@ -31,14 +32,23 @@ def about():
     return render_template('about.html', title = 'About')
 
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register', methods =['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title = 'Register', form = form)
 
-@app.route('/login')
+@app.route('/login', methods = ['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "test@gmail.com" and form.password.data == "password":
+            flash(f'You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash(f'Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title = 'Login', form = form)
 
 
